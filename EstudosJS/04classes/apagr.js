ac5b23ca91694429b03faf557ce8229283e71555
PatolinoBR007 <minehero023@gmail.com>
2023-10-27 12:32:31 -0300

    estudos de estrutura de dados

--- EstudosJS/04classes/apagr.js
+++
@@ -1,5 +0,0 @@
-for (let c = 0; c < 10; c++) {
-    setTimeout(() => {
-        console.log(c)
-    }, 10000);
-}
---
+++ EstudosJS/05data-estructure/estructureddata.js
@@ -0,0 +1,9 @@
+const tags = new Set([])
+
+tags.add('js')
+tags.add('javasrypt')
+tags.add('js')
+
+const result = Array.from(tags);
+
+console.log(result);  []
