9ab97c9f97108abed71f0919f80198aa02fb60e8
PatolinoBR007 <minehero023@gmail.com>
2023-11-16 18:25:33 -0300

    trabalhando com classes

---
+++ EstudosJS/00praticas/bolinhas/index.js
@@ -0,0 +1,39 @@
+const palco = document.getElementById('palco')
+const num_objetos = document.getElementById('num_objetos')
+const txt_qtde = document.getElementById('txt_qtde')
+const btn_qtde = document.getElementById('btn_qtde')
+const btn_add = document.getElementById('btn_add')
+const btn_remover = document.getElementById('btn_remover')
+
+let larguraPalco = palco.offsetWidth
+let alturaPalco = palco.offsetHeight
+let bolas = []
+let numBola = 0
+
+class Bola{
+    constructor() {
+        this.tam = Math.floor(Math.random()*10)+ 10
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
+        this.posX = Math.floor(Math.random()*larguraPalco-this.tam)
+        this.posy = Math.floor(Math.random()*alturaPalco-this.tam)
+    }
+
+}
+
+window.addEventListener("resize", (evt)=> {
+    larguraPalco = palco.offsetWidth
+    alturaPalco = palco.offsetHeight
+})
+btn_add.addEventListener("click", (evt)=> {
+    const qtde = txt_qtde.value
+    for (let i = 0; i < qtde; i++) {
+        //instanciar novas bolas
+    }
+})
+btn_remover.addEventListener("click", (evt)=> {
+    bolas.map((b)=> {
+        //remover bolas
+    })
+})
