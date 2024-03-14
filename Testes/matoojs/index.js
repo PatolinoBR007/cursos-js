0a75c57ae33fec665b27182ac75cae4a3b35abd0
PatolinoBR007 <minehero023@gmail.com>
2024-03-14 14:14:44 -0300

    aa

---
+++ Testes/matoojs/index.js
@@ -0,0 +1,18 @@
+
+var n2 = [10, 20, 30, "mato", true]
+
+console.log(n2[3]) 
+
+var obj = {nome: "mato", 
+            idade: 10101001010190,
+            trabalha: true}
+
+let c = 0
+while (c < 10) {
+    console.log("bem vindo")
+
+    c++
+}
+
+
+
