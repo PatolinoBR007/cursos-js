0163c5809eaf4413e1b71eb2e2c36933e15ab870
PatolinoBR007 <minehero023@gmail.com>
2024-05-24 15:13:33 -0300

    cronometro

---
+++ EstudosJS/00praticas/cronometro/scrypt.js
@@ -0,0 +1,38 @@
+const timer = document.getElementById('timer')
+const btn = [...document.querySelectorAll('.btn')]
+
+let intervalo = null
+let tmpini = null
+
+
+const contador = ()=>{
+    const tmpatual = Date.now()
+    let cont = tmpatual - tmpini
+    let seg = cont/1000
+    timer.innerHTML = converter(seg)
+}
+
+const converter = (seg)=>{
+    const hora = Math.floor(seg/3600)
+    const resto = seg%3600
+    const minuto = Math.floor(resto/60)
+    const segundo = Math.floor(resto%60)
+    const ft = tempo=>tempo<10?"0"+tempo:tempo
+    return `${ft(hora)}:${ft(minuto)}:${ft(segundo)}`
+}
+
+
+
+btn.map((el)=>{
+    el.addEventListener('click',(evt)=>{
+        if (el.id === 'btn_iniciar') {
+            tmpini = Date.now()
+            intervalo = setInterval(contador,1000)
+        } else if (el.id === 'btn_parar') {
+            clearInterval(intervalo)
+        } else if (el.id === 'btn_zerar'){
+            tmpini = Date.now()
+        
+        }
+    })
+})
---
+++ EstudosJS/13others/topo/topo.js
@@ -0,0 +1,23 @@
+let head = document.head
+const body = document.body
+
+const estilo = '<link rel="stylesheet" href="topo.css"></link>'
+head.innerHTML += estilo
+
+const topo = document.createElement('div')
+topo.setAttribute('id','topo')
+topo.setAttribute('class','topo')
+body.prepend(topo)
+
+const logo = 
+    "<div id='logo' class='logo'>"+
+      "<img src='OIP.jfif'/>"+
+    "</div>"
+topo.innerHTML+=logo
+    
+const login =
+    "<div id='login' class='login'>"+
+      "<p id='matricula'>Matricula: <span></span></p>"+
+      "<p id='nome'>Nome: <span></span></p>"+
+    "</div>"
+topo.innerHTML += login
