e735f6d37b92e04181d5c222f27af62afe79c6ed
PatolinoBR007 <minehero023@gmail.com>
2024-06-04 14:32:52 -0300

    login5

--- EstudosJS/13others/data-grid-view/gridview.js
+++ EstudosJS/13others/data-grid-view/gridview.js
@@ -1,1 +1,26 @@
-import {  } from "module";
+const configdgv ={
+    endpoint: "produtos.json",
+    idDestino: "dgvdados"
+}
+
+const dgv =(configdgv)=>{
+    const dgvDados =document.getElementById(configdgv.idDestino)
+    fetch(configdgv.endpoint).then(res=>res.json()).then(res=>{
+        res.map(el=>{ 
+            const dadosArr = [el.id, el.produto, el.marca, el.modelo, "D E V"]
+
+            const divlinha = document.createElement('div')
+            divlinha.setAttribute('class','dgvlinha')
+            dgvDados.appendChild(divlinha)
+            
+            for(let cont=0; cont<5; cont++){
+                const divDados = document.createElement('div')
+                divDados.setAttribute('class',`c${cont+1}`)
+                divDados.innerHTML= dadosArr[cont]
+                divlinha.appendChild(divDados)
+            }  
+        })
+    })
+}
+
+dgv(configdgv)
